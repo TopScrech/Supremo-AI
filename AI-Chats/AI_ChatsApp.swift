90f9c30119cf7d9d3b9997094c9ac5b0495b5001
TopScrech <sergei_saliukov@icloud.com>
2026-04-24 18:16:20 +0200

    Initial Commit

---
+++ AI-Chats/AI_ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatsApp.swift
+//  AI-Chats
+//
+//  Created by Sergei Saliukov on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
