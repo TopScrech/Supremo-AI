d96112643c0f9e08a5c0e4a05a6c90ecdfa6762a
TopScrech <sergei_saliukov@icloud.com>
2026-04-24 21:29:47 +0200

    -

---
+++ Supremo-AI/Nav/LLMChatApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct LLMChatApp: App {
+    @State private var appModel = ChatAppModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            AppContainer()
+                .environment(appModel)
+        }
+    }
+}
