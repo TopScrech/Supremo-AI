4227c13283f297b1fc7ff66c5f01f84b4b8abd48
TopScrech <sergei_saliukov@icloud.com>
2026-04-29 13:30:22 +0200

    Added Phi-4 models

--- Supremo-AI/Services/ModelCatalog.swift
+++ Supremo-AI/Services/ModelCatalog.swift
@@ -18,6 +18,9 @@ struct ModelCatalog {
         model(familyName: "Ministral 3 3B Instruct", fileName: "Ministral-3-3B-Instruct-2512-Q4_K_M.gguf", urlString: "https://huggingface.co/lmstudio-community/Ministral-3-3B-Instruct-2512-GGUF/resolve/main/Ministral-3-3B-Instruct-2512-Q4_K_M.gguf?download=true", inference: .mistral),
         model(familyName: "NSFW 13B SFT", fileName: "NSFW_13B_sft.Q2_K.gguf", urlString: "https://huggingface.co/mzwing/NSFW_13B_sft-GGUF/resolve/main/NSFW_13B_sft.Q2_K.gguf?download=true", inference: .baichuan),
         model(familyName: "Phi-3.5 mini instruct", fileName: "Phi-3.5-mini-instruct-Q4_K_S.gguf", urlString: "https://huggingface.co/bartowski/Phi-3.5-mini-instruct-GGUF/resolve/main/Phi-3.5-mini-instruct-Q4_K_S.gguf?download=true", inference: .phi),
+        model(familyName: "Phi-4 Mini Reasoning", fileName: "Phi-4-mini-reasoning-Q4_K_M.gguf", urlString: "https://huggingface.co/lmstudio-community/Phi-4-mini-reasoning-GGUF/resolve/main/Phi-4-mini-reasoning-Q4_K_M.gguf?download=true", inference: .phi),
+        model(familyName: "Phi-4 Reasoning Plus", fileName: "Phi-4-reasoning-plus-Q4_K_M.gguf", urlString: "https://huggingface.co/lmstudio-community/Phi-4-reasoning-plus-GGUF/resolve/main/Phi-4-reasoning-plus-Q4_K_M.gguf?download=true", inference: .phi),
+        model(familyName: "Phi-4", fileName: "phi-4-Q4_K_M.gguf", urlString: "https://huggingface.co/lmstudio-community/phi-4-GGUF/resolve/main/phi-4-Q4_K_M.gguf?download=true", inference: .phi),
         model(familyName: "Bunny 1.0 4B", fileName: "Bunny-v1_0-4B-Q4_K_S.gguf", urlString: "https://huggingface.co/guinmoon/Bunny-v1_0-4B-GGUF/resolve/main/Bunny-v1_0-4B-Q4_K_S.gguf?download=true", inference: .llava),
         model(familyName: "Moondream 2", fileName: "moondream2-text-model-f16.gguf", urlString: "https://huggingface.co/moondream/moondream2-gguf/resolve/main/moondream2-text-model-f16.gguf?download=true", inference: .moondream)
     ]
